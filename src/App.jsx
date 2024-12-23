c2e447e24411a293c9c87dd78c3fb307522f1cfa
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2024-12-23 15:37:02 -0800

    include not found page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Resources from './pages/Resources';
 import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/resources" element={<Resources />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound/>} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
