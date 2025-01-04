d7337548dac964939f12ed0e3212f164fb96f0b7
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2025-01-04 13:09:21 -0800

    limit basemapgallery list

--- src/components/SidePanel/BasemapGallery.jsx
+++ src/components/SidePanel/BasemapGallery.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import "@arcgis/map-components/dist/components/arcgis-basemap-gallery";
 import { ArcgisBasemapGallery } from "@arcgis/map-components-react";
+import Basemap from "@arcgis/core/Basemap";
 import {
     CalcitePanel,
 } from "@esri/calcite-components-react";
 
 const BasemapGallery = (props) => {
     const { referenceElement } = props;
+    const baseMapSources = [
+        Basemap.fromId("topo-vector"),
+        Basemap.fromId("satellite"),
+        Basemap.fromId("hybrid"),
+        Basemap.fromId("streets-navigation-vector")
+    ]
 
     return (<CalcitePanel
         heading="BasemapGallery"
@@ -14,7 +21,7 @@ const BasemapGallery = (props) => {
         data-panel-id="basemap"
         width-scale="l"
     >
-        <ArcgisBasemapGallery referenceElement={referenceElement} />
+        <ArcgisBasemapGallery referenceElement={referenceElement} source={baseMapSources} />
     </CalcitePanel>)
 }
 
